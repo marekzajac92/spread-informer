31287f2510e5644441d1d1cb436bae48b3bb8dab
Marek Z <marekzajac92@gmail.com>
2012-12-13 19:25:41 +0100

    Logowanie do serwera xtb

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,16 +35,7 @@ namespace SpreadInformer
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, "1,0,0,0,0,0");
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, "7,0,0,0,0,0");
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, "4,0,0,0,0,0");
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, "12.5,0,0,0,0,0");
             this.label1 = new System.Windows.Forms.Label();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -58,42 +49,14 @@ namespace SpreadInformer
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(0, 0);
-            this.chart1.Name = "chart1";
-            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            series1.Points.Add(dataPoint1);
-            series1.Points.Add(dataPoint2);
-            series1.Points.Add(dataPoint3);
-            series1.Points.Add(dataPoint4);
-            series1.YValuesPerPoint = 6;
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(428, 412);
-            this.chart1.TabIndex = 1;
-            this.chart1.Text = "chart1";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 412);
-            this.Controls.Add(this.chart1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            this.Resize += new System.EventHandler(this.Form1_Resize);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,7 +65,6 @@ namespace SpreadInformer
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,29 +22,16 @@ namespace SpreadInformer
         public Form1()
         {
             InitializeComponent();
-            g = new Graph();
-            double[] p = new double[5];
-            p[0] = 1.000;
-            p[1] = 1.505;
-            p[2] = 3.010;
-            p[3] = 2.500;
-            p[4] = 2.650;
-            g.SetPoints(p);
-            label1.Text = (((p[0] - p[0]) / (p[2] - p[0]) * (-(this.Height - 40))) + ((this.Height - 40) - 20)).ToString();
-        }
 
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            graphic = this.CreateGraphics();
-            graphic.Clear(Color.Black);
-            g.DrawGraph(graphic, this.Width, this.Height);
-        }
+            SyncAPIConnect conn = new SyncAPIConnect(ServerData.DevelopmentServers["DEV"]);
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            graphic = this.CreateGraphics();
-            graphic.Clear(Color.Black);
-            g.DrawGraph(graphic, this.Width, this.Height);
+            BaseCommand loginCmd = APICommandFactory.createLoginCommand(101195, "uul6rxe", false);
+            string responseJSON = conn.executeCommand(loginCmd.toJSONString());
+            LoginResponse resp = new LoginResponse(responseJSON);
+
+            CurrentUserDataResponse ud = APICommandFactory.executeCurrentUserDataCommand(conn, false);
+
+            label1.Text = ud.Currency;
         }
     }
 }
